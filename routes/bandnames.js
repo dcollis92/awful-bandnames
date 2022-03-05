3423b8cff925475c3d3bbdd668cdd4ab0b377d28
dcollis92 <davidphilipcollis@gmail.com>
2022-03-05 13:48:49 -0600

    update middleware

--- routes/bandnames.js
+++ routes/bandnames.js
@@ -6,6 +6,19 @@ const router = Router()
 
 // GET - localhost:3000/bandnames
 router.get('/', bandnamesCtrl.index)
+// GET - localhost:3000/bandnames/:id
+router.get("/:id", bandnamesCtrl.show)
+// GET - localhost:3000/bandnames/:id/edit
+router.get("/:id/edit", bandnamesCtrl.edit)
+// POST - localhost:3000/bandnames
+router.post("/", isLoggedIn, bandnamesCtrl.create)
+// PATCH - localhost:3000/bandnames/:id/add-rating
+router.patch("/:id/add-rating", isLoggedIn, bandnamesCtrl.addRating)
+// PUT- localhost:3000/bandnames/:id
+router.put("/:id", isLoggedIn, bandnamesCtrl.update)
+
+// DELTE - localhost:3000/bandnames/:id
+router.delete("/:id", isLoggedIn, bandnamesCtrl.delete)
 
 
 export {
